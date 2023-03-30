0e2c2c562c5e8227e3f983f52a9f3e537841455d
David Prakash Mukhiya <sherlock.41v94@gmail.com>
2023-03-29 23:59:31 -0700

    added initial project file

---
+++ heavenlysong-backend/src/main/java/com/david/heavenlysong/HeavenlysongApplication.java
@@ -0,0 +1,13 @@
+package com.david.heavenlysong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeavenlysongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeavenlysongApplication.class, args);
+	}
+
+}
