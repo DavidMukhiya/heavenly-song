009a276e8e1836bd816c899ca344c44c57e401b5
David Prakash Mukhiya <sherlock.41v94@gmail.com>
2023-04-17 06:36:06 -0700

    finished category API

--- heavenlysong-backend/src/main/java/com/david/heavenlysong/payloads/CategoryDto.java
+++ heavenlysong-backend/src/main/java/com/david/heavenlysong/payloads/CategoryDto.java
@@ -4,12 +4,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @NoArgsConstructor
 @Getter
 @Setter
 public class CategoryDto {
 
     private Integer CategoryId;
+
+    @NotBlank
+    @Size(min = 4, message = "Min size of category tittle is 4")
     private String categoryTitle;
+
+    @NotBlank
+    @Size(min = 10, message = "Min size of category tittle is 10")
     private String categoryDescription;
 }
--- heavenlysong-backend/src/main/java/com/david/heavenlysong/services/impl/CategoryServiceImpl.java
+++ heavenlysong-backend/src/main/java/com/david/heavenlysong/services/impl/CategoryServiceImpl.java
@@ -7,10 +7,12 @@ import com.david.heavenlysong.repositories.CategoryRepo;
 import com.david.heavenlysong.services.CategoryService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class CategoryServiceImpl implements CategoryService {
     @Autowired
     private CategoryRepo categoryRepo;
